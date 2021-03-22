df507fb481d7d2e241d72bc958f904b13e2b8cf5
mistyco-official <81126182+mistyco-official@users.noreply.github.com>
2021-03-22 02:17:46 +0100

    Created the file and added id

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/********
+COPYRIGHT (c) MISTY CO, SCARLETT FAHLSTRÖM & IARA FAHLSTRÖM
+
+***/
+
+#include <iostream> // My first game!
+#include <conio.h> // The new Zelda
+using namespace std;
+
+int main(){
+  
+  string static id = "xiftavtcghtnuihmse@miucce.com";
+    
+  struct Person
+  {
+    char name[50];
+    int age;
+    float salary;
+  };
+  
+} // End of file
